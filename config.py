3fc5bb9980abb3cc2779f63fc8eaf952b72eb711
Jozef Karabelly <jozefkarabelly@gmail.com>
2022-04-10 14:40:27 +0200

    current working configuration

--- config.py
+++ config.py
@@ -5,24 +5,25 @@ from dataclasses import dataclass
 class Config:
     save_path: str = './'
     n_steps: int = 4096 * 12
-    minibatch_size: int = 256
+    minibatch_size: int = 512
     n_envs: int = 12
     n_samples_m: int = 100
     save_freq: int = 120_000
 
-    lr_start: float = 2e-6
+    lr_start: float = 25e-6
     lr_final: float = 1 * 1e-6
     lr_scale: float = 1
 
-    clip_start: float = 0.2
-    clip_end: float = 0.1
-    clip_exp_slope: float = 5
+    clip_start: float = 0.55
+    clip_end: float = 0.2
+    clip_exp_slope: float = 3
 
     init_logstd: float = -0.75
 
-    ent_coef: float = 0
-    gamma: float = 0.95
+    ent_coef: float = -0.0075
+    gamma: float = 0.99
     gae_lambda: float = 0.95
     n_epochs: int = 4
 
+    seed: int = 53
     checkpoint: str = ''
