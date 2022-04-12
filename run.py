7971d9b2b78463022e919d629fa97e79c31b8640
Jozef Karabelly <jozefkarabelly@gmail.com>
2022-04-12 21:06:50 +0200

    add model path and checkpoint reset off

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 # add current working directory to the system path
 import argparse
+import random
 import sys
 from os import getcwd
 from pathlib import Path
@@ -19,16 +20,16 @@ from stable_baselines3 import PPO
 
 def train(cfg: Config) -> None:
     env = dm2gym.make(domain_name='humanoid_CMU', task_name='humanoid_tracking')
-    env = vectorize_env(env, norm_rew=True, n_envs=cfg.n_envs)
+    env = vectorize_env(env, norm_rew=True, n_envs=cfg.n_envs, seed=cfg.seed)
 
     save_path = Path().cwd() / cfg.save_path
     training_timesteps = int(cfg.n_samples_m * 1e6)
 
-    lr_schedule = lambda _: cfg.lr_start
-    clip_schedule = lambda _: cfg.clip_start
+    lr_schedule = LinearDecay(cfg.lr_start, cfg.lr_final, cfg.lr_scale).value
+    clip_schedule = ExponentialSchedule(cfg.clip_start, cfg.clip_end, cfg.clip_exp_slope).value
 
     policy_kwargs = {'net_arch': [{'vf': [1024, 512], 'pi': [1024, 512]}],
-                     'activation_fn': nn.ReLU}
+                     'activation_fn': nn.ReLU, 'log_std_init': cfg.init_logstd}
 
     if cfg.checkpoint:
         model = PPO.load(
@@ -51,22 +52,24 @@ def train(cfg: Config) -> None:
             n_steps=cfg.n_steps // cfg.n_envs,
             batch_size=cfg.minibatch_size,
             learning_rate=lr_schedule,
+            ent_coef=cfg.ent_coef,
             gamma=cfg.gamma,
-            gae_lambda=cfg.gae_lambda,
+            # gae_lambda=cfg.gae_lambda,
             n_epochs=cfg.n_epochs,
-            clip_range=clip_schedule,
+            clip_range_vf=clip_schedule, clip_range=clip_schedule,
             policy_kwargs=policy_kwargs,
             tensorboard_log=str(save_path / 'tb_logs')
         )
 
     # Save a checkpoint
+    model_path = save_path / 'models' / f'{random.randint(1, 1000)}/'
     checkpoint_callback = CheckpointCallback(
         save_freq=max(cfg.save_freq // cfg.n_envs, 1),
-        save_path=str(save_path / 'models/'),
+        save_path=str(model_path),
         name_prefix='rl_model'
     )
 
-    model.learn(total_timesteps=training_timesteps, callback=checkpoint_callback)
+    model.learn(total_timesteps=training_timesteps, callback=checkpoint_callback, reset_num_timesteps=False if cfg.checkpoint else True)
 
     model.save(str(save_path / 'model_final'))
 
@@ -74,9 +77,10 @@ def train(cfg: Config) -> None:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--save_path", type=str, required=False)
-    parser.add_argument("--batch_size", type=int, required=False)
+    parser.add_argument("--n_steps", type=int, required=False)
     parser.add_argument("--minibatch_size", type=int, required=False)
     parser.add_argument("--checkpoint", type=str, required=False)
+    parser.add_argument("--n_envs", type=int, required=False)
 
     args = parser.parse_args()
     config = Config(**{k: v for k, v in vars(args).items() if v is not None})
